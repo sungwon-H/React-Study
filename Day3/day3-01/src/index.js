a07d193ceaa9bd73dc5ce78ff0539e4c2cb4ca23
sungwon-H <bbh653@naver.com>
2020-12-20 18:08:33 +0900

    Modify index.js content in the day3-01 folder

--- Day3/day3-01/src/index.js
+++ Day3/day3-01/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Counter from './Counter'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Counter/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
